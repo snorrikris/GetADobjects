11d4af42ab3ee0d8a45853fc3d488738a4533bac
Snorri Kristjánsson <snorri@veritas.is>
2015-08-20 14:42:52 +0000

    Removed dependency on System.Drawing namespace

--- GetADobjects/GetADobjects.cs
+++ GetADobjects/GetADobjects.cs
@@ -9,7 +9,8 @@ using System.DirectoryServices.AccountManagement;
 using System.Security.Principal;
 using System.Xml;
 using System.Collections.Generic;
-using System.Drawing.Imaging;
+//using System.Drawing.Imaging;
+//using System.Drawing;
 
 /*
  * Must run this on the database
@@ -191,7 +192,8 @@ public partial class StoredProcedures
                 PropertyValueCollection prop = GetADproperty(item, "thumbnailphoto");
                 if (prop == null)
                     continue;
-                System.Drawing.Size imgsize = GetImageSize((byte[])prop[0]);
+                //System.Drawing.Size imgsize = GetImageSize((byte[])prop[0]);
+                ImgSize imgsize = ImageHeader.GetDimensions((byte[])prop[0]);
                 row = tbl.NewRow();
                 row[0] = item.Guid;
                 row[1] = imgsize.Width;
@@ -261,45 +263,45 @@ public partial class StoredProcedures
         return imgsize;
     }
 
-    private static void GetImageMetadata(byte[] imagedata, System.IO.StreamWriter file)
-    {
-        MemoryStream memstream = null;
-        System.Drawing.Image image = null;
-        try
-        {
-            // Create an Image object. 
-            memstream = new MemoryStream(imagedata);
-            image = System.Drawing.Image.FromStream(memstream);
+    //private static void GetImageMetadata(byte[] imagedata, System.IO.StreamWriter file)
+    //{
+    //    MemoryStream memstream = null;
+    //    System.Drawing.Image image = null;
+    //    try
+    //    {
+    //        // Create an Image object. 
+    //        memstream = new MemoryStream(imagedata);
+    //        image = System.Drawing.Image.FromStream(memstream);
 
-            // Get the PropertyItems property from image.
-            PropertyItem[] propItems = image.PropertyItems;
+    //        // Get the PropertyItems property from image.
+    //        PropertyItem[] propItems = image.PropertyItems;
 
-            // For each PropertyItem in the array, display the ID, type, and  
-            // length. 
-            int count = 0;
-            string ln = "";
-            foreach (PropertyItem propItem in propItems)
-            {
-                ln = "Property Item " + count.ToString();
-                ln += ",   iD: 0x" + propItem.Id.ToString("x");
-                ln += ",   type: " + propItem.Type.ToString();
-                ln += ",   length: " + propItem.Len.ToString() + " bytes";
-                file.WriteLine(ln);
-                count++;
-            }
+    //        // For each PropertyItem in the array, display the ID, type, and  
+    //        // length. 
+    //        int count = 0;
+    //        string ln = "";
+    //        foreach (PropertyItem propItem in propItems)
+    //        {
+    //            ln = "Property Item " + count.ToString();
+    //            ln += ",   iD: 0x" + propItem.Id.ToString("x");
+    //            ln += ",   type: " + propItem.Type.ToString();
+    //            ln += ",   length: " + propItem.Len.ToString() + " bytes";
+    //            file.WriteLine(ln);
+    //            count++;
+    //        }
 
-            file.WriteLine("-------------------------------------------------------");
-        }
-        catch (Exception ex)
-        {
-            file.WriteLine("Exception: " + ex.Message);
-        }
-        finally
-        {
-            if (memstream != null)
-                memstream.Close();
-        }
-    }
+    //        file.WriteLine("-------------------------------------------------------");
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        file.WriteLine("Exception: " + ex.Message);
+    //    }
+    //    finally
+    //    {
+    //        if (memstream != null)
+    //            memstream.Close();
+    //    }
+    //}
 
     [Microsoft.SqlServer.Server.SqlProcedure]
     public static void clr_GetADpropertiesToFile(SqlString ADpath, SqlString ADfilter)
@@ -846,6 +848,191 @@ public class ADcolsTable
             tbl.Columns.Add(col.ColName, col.datatype);
         }
         return tbl;
+    }
+}
+
+public struct ImgSize
+{
+    public Int32 Width;
+    public Int32 Height;
+    public ImgSize(Int32 width, Int32 height)
+    {
+        this.Width = width;
+        this.Height = height;
+    }
+};
+
+// Source: http://www.codeproject.com/Articles/35978/Reading-Image-Headers-to-Get-Width-and-Height
+/// <summary>
+/// Taken from http://stackoverflow.com/questions/111345/getting-image-dimensions-without-reading-the-entire-file/111349
+/// Minor improvements including supporting unsigned 16-bit integers when decoding Jfif and added logic
+/// to load the image using new Bitmap if reading the headers fails
+/// </summary>
+public static class ImageHeader
+{
+    const string errorMessage = "Could not recognise image format.";
+
+    private static Dictionary<byte[], Func<BinaryReader, ImgSize>> imageFormatDecoders = new Dictionary<byte[], Func<BinaryReader, ImgSize>>()
+        { 
+            { new byte[] { 0xff, 0xd8 }, DecodeJfif }, 
+            { new byte[] { 0x42, 0x4D }, DecodeBitmap }, 
+            { new byte[] { 0x47, 0x49, 0x46, 0x38, 0x37, 0x61 }, DecodeGif }, 
+            { new byte[] { 0x47, 0x49, 0x46, 0x38, 0x39, 0x61 }, DecodeGif }, 
+            { new byte[] { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A }, DecodePng },
+        };
+
+    public static ImgSize GetDimensions(byte[] imgdata)
+    {
+        MemoryStream memstream = null;
+        ImgSize imgsize = new ImgSize(0, 0);
+        try
+        {
+            memstream = new MemoryStream(imgdata);
+
+            using (BinaryReader binaryReader = new BinaryReader(memstream))
+            {
+                try
+                {
+                    imgsize = GetDimensions(binaryReader);
+                }
+                catch (ArgumentException e)
+                {
+                    //string newMessage = string.Format("{0} file: '{1}' ", errorMessage, path);
+
+                    //throw new ArgumentException(newMessage, "path", e);
+                }
+            }
+        }
+        catch (ArgumentException)
+        {
+        }
+        finally
+        {
+            if (memstream != null)
+                memstream.Close();
+        }
+        return imgsize;
+    }
+
+    /// <summary>        
+    /// Gets the dimensions of an image.        
+    /// </summary>        
+    /// <param name="path">The path of the image to get the dimensions of.</param>        
+    /// <returns>The dimensions of the specified image.</returns>        
+    /// <exception cref="ArgumentException">The image was of an unrecognised format.</exception>            
+    public static ImgSize GetDimensions(BinaryReader binaryReader)
+    {
+        int maxMagicBytesLength = 8; // imageFormatDecoders.Keys.OrderByDescending(x => x.Length).First().Length;
+        byte[] magicBytes = new byte[maxMagicBytesLength];
+        for (int i = 0; i < maxMagicBytesLength; i += 1)
+        {
+            magicBytes[i] = binaryReader.ReadByte();
+            foreach (var kvPair in imageFormatDecoders)
+            {
+                if (StartsWith(magicBytes, kvPair.Key))
+                {
+                    return kvPair.Value(binaryReader);
+                }
+            }
+        }
+
+        throw new ArgumentException(errorMessage, "binaryReader");
+    }
+
+    private static bool StartsWith(byte[] thisBytes, byte[] thatBytes)
+    {
+        for (int i = 0; i < thatBytes.Length; i += 1)
+        {
+            if (thisBytes[i] != thatBytes[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static short ReadLittleEndianInt16(BinaryReader binaryReader)
+    {
+        byte[] bytes = new byte[sizeof(short)];
+
+        for (int i = 0; i < sizeof(short); i += 1)
+        {
+            bytes[sizeof(short) - 1 - i] = binaryReader.ReadByte();
+        }
+        return BitConverter.ToInt16(bytes, 0);
+    }
+
+    private static ushort ReadLittleEndianUInt16(BinaryReader binaryReader)
+    {
+        byte[] bytes = new byte[sizeof(ushort)];
+
+        for (int i = 0; i < sizeof(ushort); i += 1)
+        {
+            bytes[sizeof(ushort) - 1 - i] = binaryReader.ReadByte();
+        }
+        return BitConverter.ToUInt16(bytes, 0);
+    }
+
+    private static int ReadLittleEndianInt32(BinaryReader binaryReader)
+    {
+        byte[] bytes = new byte[sizeof(int)];
+        for (int i = 0; i < sizeof(int); i += 1)
+        {
+            bytes[sizeof(int) - 1 - i] = binaryReader.ReadByte();
+        }
+        return BitConverter.ToInt32(bytes, 0);
+    }
+
+    private static ImgSize DecodeBitmap(BinaryReader binaryReader)
+    {
+        binaryReader.ReadBytes(16);
+        int width = binaryReader.ReadInt32();
+        int height = binaryReader.ReadInt32();
+        return new ImgSize(width, height);
+    }
+
+    private static ImgSize DecodeGif(BinaryReader binaryReader)
+    {
+        int width = binaryReader.ReadInt16();
+        int height = binaryReader.ReadInt16();
+        return new ImgSize(width, height);
+    }
+
+    private static ImgSize DecodePng(BinaryReader binaryReader)
+    {
+        binaryReader.ReadBytes(8);
+        int width = ReadLittleEndianInt32(binaryReader);
+        int height = ReadLittleEndianInt32(binaryReader);
+        return new ImgSize(width, height);
+    }
+
+    private static ImgSize DecodeJfif(BinaryReader binaryReader)
+    {
+        while (binaryReader.ReadByte() == 0xff)
+        {
+            byte marker = binaryReader.ReadByte();
+            short chunkLength = ReadLittleEndianInt16(binaryReader);
+            if (marker == 0xc0)
+            {
+                binaryReader.ReadByte();
+                int height = ReadLittleEndianInt16(binaryReader);
+                int width = ReadLittleEndianInt16(binaryReader);
+                return new ImgSize(width, height);
+            }
+
+            if (chunkLength < 0)
+            {
+                ushort uchunkLength = (ushort)chunkLength;
+                binaryReader.ReadBytes(uchunkLength - 2);
+            }
+            else
+            {
+                binaryReader.ReadBytes(chunkLength - 2);
+            }
+        }
+
+        throw new ArgumentException(errorMessage);
     }
 }
 
